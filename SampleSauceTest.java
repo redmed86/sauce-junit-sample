a528026f9b31d3ef98bbfe0fadaf993b00eb503c
kristiansl <kristianmeier@kristians-MacBook-Air.local>
2015-04-14 13:44:35 -0700

    New UI for Sauce..

--- SampleSauceTest.java
+++ SampleSauceTest.java
@@ -132,7 +132,7 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
             capabilities.setCapability(CapabilityType.VERSION, version);
         }
         capabilities.setCapability(CapabilityType.PLATFORM, os);
-        capabilities.setCapability("name", "Simple Brower Tests 3");
+        capabilities.setCapability("name", "Browser Test 4");
         this.driver = new RemoteWebDriver(
                 new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
                 capabilities);
