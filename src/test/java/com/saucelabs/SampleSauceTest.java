de74f918696d4e3fd2613098882bca93a3366366
kristiansl <kristian@saucelabs.com>
2015-08-10 14:52:13 -0700

    demo

--- src/test/java/com/saucelabs/SampleSauceTest.java
+++ src/test/java/com/saucelabs/SampleSauceTest.java
@@ -176,7 +176,7 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
         driver.findElement(By.id("login-pass")).sendKeys("somepasas");
         driver.findElement(By.id("login-button")).click();
         Thread.sleep(5000);
-      
+       
  
     } 
 
