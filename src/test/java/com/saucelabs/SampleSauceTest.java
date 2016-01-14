2325f719787cbe4bbc4276458621a735574ee1ab
kristiansl <kristian@saucelabs.com>
2016-01-14 10:36:10 -0800

    fixing error

--- src/test/java/com/saucelabs/SampleSauceTest.java
+++ src/test/java/com/saucelabs/SampleSauceTest.java
@@ -53,7 +53,7 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
 
     @Rule public TestName name = new TestName() {
         public String getMethodName() {
-                return String.format("%s : (%s %s %s)", super.getMethodName(), os, browser, verifyingrsion);
+                return String.format("%s : (%s %s %s)", super.getMethodName(), os, browser, version);
         };
     };
     /**
